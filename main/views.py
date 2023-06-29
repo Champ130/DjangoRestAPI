114e68d9bf1a92461e366b4f93c70eb020443703
Champ-max <gamerchamp84@gmail.com>
2023-06-29 13:31:46 +0530

    fourth push

--- main/views.py
+++ main/views.py
@@ -2,16 +2,9 @@ from rest_framework.response import Response
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 
-from main.models import Category
+from.models import Category
 
 # Create your views here.
-@api_view(['GET'])
-def create_category(request):
-    return Response({"Sucess":"Category added Sucessfully"})
-
-
-
-
 @api_view(['POST'])
 def create_category(request):
     category_name = request.data.get('category')
