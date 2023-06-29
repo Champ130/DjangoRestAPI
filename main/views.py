25fffce9134e9325cefe52ba8a4f176fa443bf7b
Champ-max <gamerchamp84@gmail.com>
2023-06-29 12:15:09 +0530

    Second push

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
+from rest_framework.response import Response
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def create_category(request):
+    return Response({"sucess":"Category added Sucessfully"})
