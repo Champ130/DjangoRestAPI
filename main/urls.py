545d62819e93d575af72d2869cffa71adacef5f4
Champ-max <gamerchamp84@gmail.com>
2023-07-01 00:04:31 +0530

    third push

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
-
 from django.urls import path
-from .views import create_category,delete_category
+from .views import create_category,delete_category, get_category, update_category
 urlpatterns = [
     path("create-category/", create_category, name="create-category"),
+    path("get-category/", get_category, name="get-category"),
     path("delete-category/<int:id>", delete_category, name="delete-category"),
+    path("update-category/<int:id>", update_category, name="update-category"),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
-from rest_framework.response import Response
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-
+from rest_framework.response import Response
+import json
 from.models import Category
 
 # Create your views here.
@@ -17,4 +17,22 @@ def create_category(request):
 def delete_category(request,id):
     Category.objects.filter(id=id).delete()
     cat_data = Category.objects.all().values()
+    return Response({"data": cat_data})
+
+
+
+
+@api_view(['GET'])
+def get_category(request):
+    cat_data = Category.objects.all().values()
+    return Response({"success":"category added","data2":cat_data})
+
+
+
+@api_view(["PATCH"])
+def update_category(request,id):
+    Category.objects.get(id=id)
+    cat_data = Category.objects.get(id=id)
+    with open ("update_category","w") as f:
+        json.dump(update_category,f)
     return Response({"data": cat_data})
