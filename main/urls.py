5c45e0f284646188732cb761118e6ae38201780c
Champ-max <gamerchamp84@gmail.com>
2023-07-02 08:52:42 +0530

    intial push

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_category,delete_category, get_category, update_category
+from .views import CategoryView, create_category,delete_category, get_category,  updates_category
 urlpatterns = [
     path("create-category/", create_category, name="create-category"),
     path("get-category/", get_category, name="get-category"),
     path("delete-category/<int:id>", delete_category, name="delete-category"),
-    path("update-category/<int:id>", update_category, name="update-category"),
+    path("updates-category/<int:id>", updates_category, name="updates-category"),
+    path("category/",CategoryView.as_view(),name="category-views")
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,APIView
 from rest_framework.response import Response
 import json
 from.models import Category
@@ -29,10 +29,32 @@ def get_category(request):
 
 
 
-@api_view(["PATCH"])
-def update_category(request,id):
-    Category.objects.get(id=id)
-    cat_data = Category.objects.get(id=id)
-    with open ("update_category","w") as f:
-        json.dump(update_category,f)
-    return Response({"data": cat_data})
+# @api_view(["PATCH"])
+# def update_category(request,id):
+#     Category.objects.get(id=id)
+#     cat_data = Category.objects.get(id=id)
+#     with open ("update_category","w") as f:
+#         json.dump(update_category,f)
+#     return Response({"data": cat_data})
+
+
+
+@api_view(['PUT'])
+def updates_category(request,id):
+    category = Category.objects.get(id=id)
+    category.name =request.data.get('name')
+    category.save()
+    cat_data = Category.objects.all().values()
+    return Response({'data':cat_data})
+
+
+class CategoryView(APIView):
+    def get(self,request):
+        category =Category.objects.all().values()
+        return Response({'category':category})
+    
+    def post(self,request):
+        name=request.data.get('category-name')
+        Category.objects.create(name=name)
+        category =Category.objects.all().values()
+        return Response({'category':category})
